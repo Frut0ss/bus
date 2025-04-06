d9e4169f3df61f91cdb4e10d388c79380e286536
Frut0ss <alexfrutos8@gmail.com>
2025-04-06 18:06:47 +0100

    Corrections to main from Tanguys branch
    2.0

--- scenes/bus.gd
+++ scenes/bus.gd
@@ -1,27 +1,8 @@
 extends Area2D
 
+# Bus properties
 @export var speed = 700  # Pixels per second
-var stopped: bool = false
-var has_stopped: bool = false
-var at_bus_stop: bool = false
-
-func _ready():
-	add_to_group("bus")
 
 func _process(delta):
-	if not stopped:
-		position.x -= speed * delta
-
-func stop_at_bus_stop():
-	stopped = true
-	has_stopped = true
-	at_bus_stop = true
-
-func resume():
-	stopped = false
-	at_bus_stop = false
-
-# Lógica para cuando el jugador baja (puede ser llamada desde player)
-func disembark_player():
-	# Esto se puede adaptar luego
-	print("Jugador ha bajado del bus.")
+	# Simple movement - just move left
+	position.x -= speed * delta
