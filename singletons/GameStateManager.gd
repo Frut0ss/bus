f78135663c349da2ffab9f640c0ea38873e43486
tlalexandre <120526785+tlalexandre@users.noreply.github.com>
2025-04-09 18:37:42 +0100

    Created bus_stop_resource, and Transit system
    At the moment, I started by creating only the bus_stop_resource, this works as data container.
    
    Initially the bus_stop_scene is set as test_bus_stop, but this will be automatically updated when we start having bus line logic .

--- singletons/GameStateManager.gd
+++ singletons/GameStateManager.gd
@@ -4,10 +4,16 @@ extends Node
 enum GameState {MAP_VIEW, BUS_STOP, INTERIOR_BUS}
 var current_state = GameState.MAP_VIEW
 
+# Add a property to track selected stop on map
+var selected_map_stop_id = ""
+
 func _process(delta):
 	match current_state:
 		GameState.MAP_VIEW:
 			if Input.is_action_just_pressed("click"):
+				# In a real implementation, you would check if a bus stop
+				# was clicked and set selected_map_stop_id accordingly
+				selected_map_stop_id = "test_stop" # For testing
 				change_to_state(GameState.BUS_STOP)
 		GameState.BUS_STOP:
 			if Input.is_action_just_pressed("click"):
@@ -21,12 +27,14 @@ func change_to_state(new_state):
 	
 	match new_state:
 		GameState.MAP_VIEW:
-			#SceneTransitionManager.fade_to_scene("map")
+			# Clear current bus/bus stop selection when returning to map
+			TransitSystem.current_bus_line = null
 			SceneTransitionManager.change_scene("map")
 		GameState.BUS_STOP:
-			#SceneTransitionManager.fade_to_scene("bus_stop")
+			# Set the current bus stop in TransitSystem
+			if selected_map_stop_id != "":
+				TransitSystem.set_current_bus_stop(selected_map_stop_id)
 			SceneTransitionManager.change_scene("bus_stop")
 		GameState.INTERIOR_BUS:
-			#SceneTransitionManager.fade_to_scene("bus_stop")
+			# The current bus should already be set when boarding
 			SceneTransitionManager.change_scene("interior_bus")
-		# Handle other states...
